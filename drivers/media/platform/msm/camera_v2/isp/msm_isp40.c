edb561a965a3c0f9af7b104aed8affb2021848d0
Meera Gande <mgande@codeaurora.org>
2016-02-24 18:12:04 +0530

    msm: isp: Enable testgen recording for 24M 30fps
    As violations were observed at streamOn followed
    by SOF freeze when testgen is enabled after camif,
    changes are made to enable testgen before camif.
    
    CRs-Fixed: 982807
    Change-Id: Ib7c3adb1f23fd6ae763383fda152ac63e4e00f74
    Signed-off-by: Meera Gande <mgande@codeaurora.org>

--- drivers/media/platform/msm/camera_v2/isp/msm_isp40.c
+++ drivers/media/platform/msm/camera_v2/isp/msm_isp40.c
@@ -1502,6 +1502,8 @@ static void msm_vfe40_update_camif_state(struct vfe_device *vfe_dev,
 		msm_camera_io_w_mb(val & ~(1 << 8), vfe_dev->vfe_base + 0x464);
 		msm_camera_io_w_mb((update_state == DISABLE_CAMIF ? 0x0 : 0x6),
 				vfe_dev->vfe_base + 0x2F4);
+	} else if (update_state == DISABLE_CAMIF) {
+		msm_camera_io_w_mb(0x0, vfe_dev->vfe_base + 0x2F4);
 		vfe_dev->axi_data.src_info[VFE_PIX_0].active = 0;
 		/* testgen OFF*/
 		if (vfe_dev->axi_data.src_info[VFE_PIX_0].input_mux == TESTGEN)
