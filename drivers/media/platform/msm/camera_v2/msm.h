369904c44763c60686874600a330c6147b0a8c81
Jignesh Mehta <jigneshm@codeaurora.org>
2016-04-22 18:06:47 -0700

    msm:camera: Enable/Disable camera daemon
    Camera daemon process is moving to mediaserver
    process. MCT will send contol command
    to indicate camera daemon is present or not.
    
    For backward compatibility, add check
    so kernel can support camera daemon present
    and camera daemon not present.
    
    Change-Id: Ia9e45f539bd5ec0d2edfe9bfb90942d88b6d30da
    Signed-off-by: Jignesh Mehta <jigneshm@codeaurora.org>

--- drivers/media/platform/msm/camera_v2/msm.h
+++ drivers/media/platform/msm/camera_v2/msm.h
@@ -43,6 +43,8 @@ extern bool is_daemon_status;
 #define CAMERA_DISABLE_PC_LATENCY 100
 #define CAMERA_ENABLE_PC_LATENCY PM_QOS_DEFAULT_VALUE
 
+extern bool is_daemon_status;
+
 struct msm_video_device {
 	struct video_device *vdev;
 	atomic_t opened;
@@ -120,6 +122,7 @@ static inline bool msm_is_daemon_present(void)
 
 void msm_pm_qos_update_request(int val);
 
+void msm_pm_qos_update_request(int val);
 int msm_post_event(struct v4l2_event *event, int timeout);
 int  msm_create_session(unsigned int session, struct video_device *vdev);
 int msm_destroy_session(unsigned int session_id);
